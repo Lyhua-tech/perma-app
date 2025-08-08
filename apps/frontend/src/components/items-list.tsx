6fa5abf8b2b3c97c7c52afa49c04482dbf41e2ec
Lyhua-tech <slyhour69@gmail.com>
2025-08-08 16:01:13 +0700

    add type to the value

--- apps/frontend/src/components/items-list.tsx
+++ apps/frontend/src/components/items-list.tsx
@@ -2,18 +2,27 @@
 
 import Link from "next/link";
 
-const ItemsList = ({ values }) => {
+interface Item {
+  id: string | number;
+  name: string;
+  ownerName?: string;
+}
+
+interface Props {
+  values: Item[];
+}
+
+const ItemsList = ({ values }: Props) => {
   return (
     <>
       <ul>
-        {values?.map((item, index) => {
+        {values?.map((item: Item, index: number) => {
           return (
             <li key={index}>
               <Link href={`/inventory/${item.id}`}>
                 <div className="flex bg-emerald-500 p-5 gap-5 m-3 justify-between">
                   <h1>{item.name}</h1>
                   <h2>Manage by: {item.ownerName || "hua hua"}</h2>
-                  
                 </div>
               </Link>
             </li>
